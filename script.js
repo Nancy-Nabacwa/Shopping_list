28d53bcca36810f2993a5afbe6a37310167d6eef
cheshari-pearl <chesharipearl@gmail.com>
2024-05-19 19:09:03 +0300

    added changes to script.js file

---
+++ script.js
@@ -0,0 +1,7 @@
+const redirect = document.getElementById('redirectButton');
+ 
+function redirectToPage(){
+    window.location.href = "lists.html"
+
+}
+redirectButton.addEventListener('click', redirectToPage)
